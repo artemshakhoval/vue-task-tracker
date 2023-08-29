464cb4cae2458add69c0e5b2c5485e4d0e5bfd38
shah <artemshakhoval@gmail.com>
2023-08-29 14:47:22 +0300

    add header block

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,43 @@
 <template>
- <h1>Vue Task Tracker</h1>
+  <div class="container">
+    <Header title="Vue Task Tracker"/>
+  </div>
 </template>
 
 <script>
-
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    Header,
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+.container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 30px 80px;
+  border: 1px black solid;
+}
+
+.btn {
+  padding: 8px;
+  background: black;
+  color: rgb(239, 236, 236);
+  border: 1px solid;
+  border-radius: 4px;
+  cursor: pointer;
+    }
+.btn:focus {
+  outline: none;
 }
 </style>
---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+    <button :style="{background: color}" class="btn">{{ text }}</button>
+</template>
+
+<script>
+    export default {
+        name: 'Button',
+        props: {
+            text: String,
+            color: String,
+        }
+    }
+</script>
+
+<style scope>
+    /* button {
+        padding: 8px;
+        background: rgb(15, 124, 15);
+        color: rgb(239, 236, 236);
+        border: 1px rgb(15, 124, 15) solid;
+        border-radius: 2px;
+        cursor: pointer;
+    } */
+</style>
